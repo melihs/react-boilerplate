19cc1e5568be677d0903b6b045e0f86ce8177046
melih şahin <melihsahin24@gmail.com>
2023-02-19 22:02:38 +0300

    Remove components and refactor main components

---
+++ template/src/components/pages/dashboard/DashboardContext.js
@@ -0,0 +1,4 @@
+import { createContext, useContext } from "react";
+const DashboardContext = createContext();
+
+export { DashboardContext, useContext };
---
+++ template/src/components/templates/notFound/NotFound.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+const NotFound = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center flex flex-col items-center w-full mt-60">
+      <h1 className="text-3xl font-bold mt-0 mb-6">{t("notFound.title")}</h1>
+      <h3 className="text-xl mb-8">{t("notFound.description")}</h3>
+      <Link to="/" className="flex justify-center">
+        <button className="text-center mt-3 font-medium text-blue hover:underline decoration-transparent hover:decoration-inherit transition duration-300 ease-in-out">
+          {t("notFound.ana_sayfaya_geri_dön")}
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
